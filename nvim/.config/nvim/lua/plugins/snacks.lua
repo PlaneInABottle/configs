2c7a596fd45df48d263fe1212967d055a913792b
Mirza <y.mirza.altay@gmail.com>
2026-02-15 20:32:56 +0300

    Update nvim explorers

--- nvim/.config/nvim/lua/plugins/snacks.lua
+++ nvim/.config/nvim/lua/plugins/snacks.lua
@@ -1,6 +1,23 @@
 return {
   "snacks.nvim",
+  keys = {
+    { "<leader>e", false },
+    { "<leader>E", false },
+    { "<leader>fe", false },
+    { "<leader>fE", false },
+  },
   opts = {
+    picker = {
+      sources = {
+        files = {
+          hidden = true, -- show dotfiles
+          ignored = false, -- don't hide .gitignore files
+        },
+        grep = {
+          hidden = true,
+        },
+      },
+    },
     dashboard = {
       preset = {
         header = [[
